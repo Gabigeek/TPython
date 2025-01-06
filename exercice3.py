56173618b4721e807eb180e5b8872d30deb0ce90
Gabigeek <gabigeek22@gmail.com>
2025-01-06 13:49:06 +0100

    Ajout exercice 3

---
+++ exercice3.py
@@ -0,0 +1,5 @@
+saisie1 = input("1er nombre: ")
+saisie2 = input("2ème nombre: ")
+print ("La première saisie est: ", saisie1, "et la deuxième saisie est: ", saisie2)
+resultat = int(saisie1) + int(saisie2)
+print("Le résultat de la somme des deux saisies est: ", resultat)
