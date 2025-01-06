0888bed42aea6c79025c39cab638b6be6239add7
Gabigeek <gabigeek22@gmail.com>
2025-01-06 12:12:43 +0100

    update exercice2.py to include dictionary creation and modification

--- exercice2.py
+++ exercice2.py
@@ -1,3 +1,9 @@
 list1 = ['Bronze','Argent','Or']
 list1[1] = 'Platine'
 
+dictionnaire = {"rang": list1}
+print(dictionnaire)
+
+dictionnaire = {"rang": list1, "niveau": 30}
+dictionnaire["niveau"] = int(dictionnaire["niveau"]/2)
+print(dictionnaire)
